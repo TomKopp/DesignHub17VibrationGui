20b5be0bc8daeeb0cade8fa6f4ab529ddfb2cec0
Tom Kopp <witzbould@live.com>
2018-04-21 16:29:34 +0200

    add listeners for in/decrease intensity update properties and submit method

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const mapInputToOutputRange = (val, inputMin, inputMax, outputMin, outputMax) =>
 	return (outputMax - outputMin) * (val - inputMin) / (inputMax - inputMin) + outputMin;
 };
 
+const url = `ws://${window.location.hostname}:${window.location.port}`;
 const ws = new WebSocket('ws://127.0.0.1:3000');
 
 
@@ -9,48 +10,65 @@ const Motor = function Motor(id, DomNode) {
 	this._node = DomNode;
 	this._progress = this._node.querySelector('progress');
 	this._id = id;
-	this._intensity = 0;
+	this._intensity;
 
 	this._node.addEventListener('click', () => this.submit());
+	this._node.previousElementSibling.addEventListener('click', () => this.decreaseBy(5));
+	this._node.nextElementSibling.addEventListener('click', () => this.increaseBy(5));
+
+	this.intensity = 50;
 }
 
 Object.defineProperties(Motor.prototype, {
 	id: {
-		get: function () {
+		get() {
 			return this._id;
-		},
-		set: undefined
+		}
 	},
 	intensity: {
-		get: function () {
+		get() {
 			return this._intensity;
 		},
-		set: function (value) {
+		set(value) {
 			this._intensity = value;
 			this._progress.value = value;
 			this._progress.textContent = `${value}%`;
 		}
 	},
 	pwm: {
-		get: function () {
+		get() {
 			const inputMin = 0;
 			const inputMax = 100;
 			const outputMin = 75;
 			const outputMax = 255;
 			return mapInputToOutputRange(this._intensity, inputMin, inputMax, outputMin, outputMax);
-		},
-		set: undefined
+		}
 	}
 });
 
 Motor.prototype.submit = function submit() {
+	const payload = { type: 'VIBRATION', payload: `${this.id}=${this.pwm}` };
 	if (ws.readyState === ws.OPEN) {
-		ws.send(`${this.id}=${this.pwm}`);
+		ws.send(JSON.stringify(payload));
 	}
-	console.log(`${this.id}=${this.pwm}`);
+	console.log(payload);
 }
 
+Motor.prototype.increaseBy = function increaseBy(val) {
+	this.intensity += val;
+}
 
-const bla = new Motor(1, document.getElementById('motor1'));
+Motor.prototype.decreaseBy = function decreaseBy(val) {
+	this.intensity -= val;
+}
 
-console.log(bla);
+const m0 = new Motor(0, document.getElementById('motor0'));
+const m1 = new Motor(1, document.getElementById('motor1'));
+const m2 = new Motor(2, document.getElementById('motor2'));
+const m3 = new Motor(3, document.getElementById('motor3'));
+const m4 = new Motor(4, document.getElementById('motor4'));
+const m5 = new Motor(5, document.getElementById('motor5'));
+const m6 = new Motor(6, document.getElementById('motor6'));
+const m7 = new Motor(7, document.getElementById('motor7'));
+
+console.log(m0);
